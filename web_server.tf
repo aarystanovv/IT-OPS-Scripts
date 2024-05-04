fbed12e35733307338993dfe2aff0f87c909a8d3
Temirlan Arystan <74903776+aarystanovv@users.noreply.github.com>
2024-05-04 11:40:07 +0500

    Add files via upload

---
+++ web_server.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Web Server"
+  }
+}
